d1b5503c8cfb035ecc5b06324aeb780b8c18477e
Xiaoyuan Wang <wangxyd@qq.com>
2024-04-17 08:49:19 +0800

    add reply_reference_query

--- README.md
+++ README.md
@@ -16,11 +16,15 @@
 ```json
 {
   "group_at_probability": 0,
-  "add_quoter_nickname": true
+  "add_quoter_nickname": true,
+  "reply_reference_query": true
 }
 ```
 
 在以上配置项中：
 
 - `group_at_probability`: 群聊中回复消息时at对方的概率。取值范围0~1，0每次都不@对方，1每次都@对方；
-- `add_quoter_nickname`: 是否在群聊中开启识别对方身份的功能。值为`true`时会将对方的微信昵称添加到对话内容中，然后COW会使用`xxx对你说：原来的对话内容`向后端模型发起查询请求，以便模型识别到对方的身份。
+- `add_quoter_nickname`: 是否在群聊中开启识别对方身份的功能。值为`true`时会将对方的微信昵称添加到对话内容中，然后CoW会使用`xxx对你说：原来的对话内容`向后端模型发起查询请求，以便模型识别对方的身份；
+- `reply_reference_query`: 是否回复包含引用的消息。CoW默认忽略引用的消息，值为`true`时会将对话内容中引用消息的标记"」\n- - - - - - -"修改为"前面「」中的内容是引用的消息，新的问题是："，然后CoW会响应引用的信息。
+
+如果您有任何更好的想法或建议，都非常欢迎您积极提出哦~~~
--- ipartment.py
+++ ipartment.py
@@ -1,7 +1,7 @@
 # encoding:utf-8
 
-import json
 import os
+import re
 import random
 
 import plugins
@@ -9,36 +9,44 @@ from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
 from common.log import logger
 from plugins import *
-from config import global_config
 
 @plugins.register(
     name="iPartment",
     desire_priority=88,
     hidden=True,
     desc="爱情公寓专享插件。",
-    version="1.0",
+    version="1.1",
     author="空心菜",
 )
 class iPartment(Plugin):
     def __init__(self):
         super().__init__()
         try:
-            # load config
+            # 加载配置
             conf = super().load_config()
             if not conf:
                 raise Exception("[iPartment] config.json not found")
-            self.group_at_probability = conf.get('group_at_probability', 1)
-            self.add_quoter_nickname = conf.get('add_quoter_nickname', False)
-            if self.group_at_probability < 1:
-                self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
-                logger.info("[iPartment] group_at_probability is on.")
+            # 回复包含引用的消息
+            self.reply_reference_query = conf.get('reply_reference_query', False)
+            if self.reply_reference_query:
+                logger.info("[iPartment] reply_reference_query is on.")
             else:
-                logger.info("[iPartment] group_at_probability is off because of group_at_probability>=1.")
+                logger.info("[iPartment] reply_reference_query is off.")
+            # 群聊中识别对方身份
+            self.add_quoter_nickname = conf.get('add_quoter_nickname', False)
             if self.add_quoter_nickname:
-                self.handlers[Event.ON_DECORATE_REPLY] = self.on_decorate_reply
                 logger.info("[iPartment] add_quoter_nickname is on.")
             else:
                 logger.info("[iPartment] add_quoter_nickname is off.")
+            if self.add_quoter_nickname or self.reply_reference_query:
+                self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
+            # 回复消息时随机at
+            self.group_at_probability = conf.get('group_at_probability', 1)
+            if self.group_at_probability < 1:
+                self.handlers[Event.ON_DECORATE_REPLY] = self.on_decorate_reply
+                logger.info("[iPartment] group_at_probability is on.")
+            else:
+                logger.info("[iPartment] group_at_probability is off because of group_at_probability>=1.")
             logger.info("[iPartment] inited")
         except Exception as e:
             logger.warn("[iPartment] init failed, ignore.")
@@ -49,14 +57,23 @@ class iPartment(Plugin):
             return
         content = e_context["context"].content
         logger.debug("[iPartment] on_handle_context. content: %s" % content)
-        # 群聊中识别对方身份，并在提示词开头添加”xxx对你说：“
-        if self.add_quoter_nickname and e_context["context"].get("isgroup", False):
-            actual_user_nickname = e_context["context"]["msg"].actual_user_nickname
-            add_prefix = f"{actual_user_nickname}对你说："
-            content = add_prefix + content
-            logger.debug(f"[iPartment] {add_prefix} have been added.")
-        if e_context["context"].content != content:
-            e_context["context"].content = content
+        try:
+            # 回复包含引用的消息
+            if self.reply_reference_query and "」\n- - - - - - -" in content:
+                content = re.sub(r'」\n(- ){6,}-', '」\n前面「」中的内容是引用的消息，新的问题是：', content)
+                logger.debug(f"[iPartment] reference query have been modified.")
+            # 群聊中识别对方身份，并在提示词开头添加”xxx对你说：“
+            if self.add_quoter_nickname and e_context["context"].get("isgroup", False):
+                actual_user_nickname = e_context["context"]["msg"].actual_user_nickname
+                add_prefix = f"{actual_user_nickname}对你说："
+                # 确保修改操作的幂等性
+                if not content.startswith(add_prefix):
+                    content = add_prefix + content
+                    logger.debug(f"[iPartment] {add_prefix} have been added.")
+            if e_context["context"].content != content:
+                e_context["context"].content = content
+        except Exception as e:
+            logger.warn(f"[iPartment] error occurred: {e}.")
 
     def on_decorate_reply(self, e_context: EventContext):
         if e_context["reply"].type != ReplyType.TEXT:
@@ -69,7 +86,7 @@ class iPartment(Plugin):
                 e_context["context"]["no_need_at"] = True
             logger.debug(f"[iPartment] no_need_at is {not need_at}.")
         except Exception as e:
-            logger.debug(f"[iPartment] error occurred: {e}.")
+            logger.warn(f"[iPartment] error occurred: {e}.")
 
     def get_help_text(self, **kwargs):
         return "爱情公寓专享插件。"
